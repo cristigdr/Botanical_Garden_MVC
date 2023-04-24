118c50c25b6a1c815f1ce35a19bc32458cd30c36
Gedderth Cristian <cristi.gedderth@yahoo.com>
2023-04-24 13:57:05 +0200

    AdminView class

--- BotanicalGardenMVC/src/main/java/View/AdminView.java
+++ BotanicalGardenMVC/src/main/java/View/AdminView.java
@@ -5,6 +5,15 @@ import java.awt.*;
 
 public class AdminView extends JFrame{
     private JPanel mainPanelAdmin;
+    private JTextField txtId;
+    private JTextField txtUser;
+    private JTextField txtPassword;
+    private JComboBox comboRole;
+    private JButton btnInsert;
+    private JButton btnUpdate;
+    private JButton btnDelete;
+    private JButton btnClean;
+    private JTable tabUser;
 
     public AdminView() {
         super();
@@ -19,5 +28,43 @@ public class AdminView extends JFrame{
 
     }
 
+    public JPanel getMainPanelAdmin() {
+        return mainPanelAdmin;
+    }
 
+    public JTextField getTxtId() {
+        return txtId;
+    }
+
+    public JTextField getTxtUser() {
+        return txtUser;
+    }
+
+    public JTextField getTxtPassword() {
+        return txtPassword;
+    }
+
+    public JComboBox getComboRole() {
+        return comboRole;
+    }
+
+    public JButton getBtnInsert() {
+        return btnInsert;
+    }
+
+    public JButton getBtnUpdate() {
+        return btnUpdate;
+    }
+
+    public JButton getBtnDelete() {
+        return btnDelete;
+    }
+
+    public JButton getBtnClean() {
+        return btnClean;
+    }
+
+    public JTable getTabUser() {
+        return tabUser;
+    }
 }
