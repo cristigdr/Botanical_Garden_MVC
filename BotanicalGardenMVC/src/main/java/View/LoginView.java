27869c0eff69c6b2af2c4a7ad23872dde2f688df
Gedderth Cristian <cristi.gedderth@yahoo.com>
2023-04-24 12:53:35 +0200

    LoginView class

--- BotanicalGardenMVC/src/main/java/View/LoginView.java
+++ BotanicalGardenMVC/src/main/java/View/LoginView.java
@@ -1,4 +1,49 @@
 package View;
 
-public class LoginView {
+import javax.swing.*;
+import java.awt.*;
+
+public class LoginView extends JFrame{
+    private JPanel mainPanelLogin;
+    private JTextField txtUser;
+    private JPasswordField txtPassword;
+    private JButton btnLogin;
+    private JButton btnGuest;
+    private JButton btnCancel;
+
+    public LoginView() {
+        super();
+        setContentPane(mainPanelLogin);
+        setTitle("Formular autentificare");
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        setSize(700, 275);
+        setMinimumSize(new Dimension(350, 150));
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }
+
+    public JPanel getMainPanelLogin() {
+        return mainPanelLogin;
+    }
+
+    public JTextField getTxtUser() {
+        return txtUser;
+    }
+
+    public JPasswordField getTxtPassword() {
+        return txtPassword;
+    }
+
+    public JButton getBtnLogin() {
+        return btnLogin;
+    }
+
+    public JButton getBtnGuest() {
+        return btnGuest;
+    }
+
+    public JButton getBtnCancel() {
+        return btnCancel;
+    }
 }
