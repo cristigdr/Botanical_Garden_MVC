14cf777c1409b586be05dba2d9f7425e4457974d
Gedderth Cristian <cristi.gedderth@yahoo.com>
2023-04-25 10:33:21 +0200

    EmployeeController constructor

--- BotanicalGardenMVC/src/main/java/Controller/EmployeeController.java
+++ BotanicalGardenMVC/src/main/java/Controller/EmployeeController.java
@@ -1,4 +1,43 @@
 package Controller;
 
+import Model.Plant;
+import Model.PlantRepository;
+import View.EmployeeView;
+import View.GuestView;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
+
 public class EmployeeController {
+    private PlantRepository plantRepo;
+    private EmployeeView empView;
+
+    public EmployeeController() {
+        this.plantRepo = new PlantRepository();
+        this.empView = new EmployeeView();
+        populateTable();
+
+        this.empView.getBtnSearch().addActionListener(e -> searchClick());
+        this.empView.getBtnRefresh().addActionListener(e -> populateTable());
+        this.empView.getBtnClean().addActionListener(e -> cleanFieldsClick());
+
+        this.empView.getBtnInsert().addActionListener(e -> insertPlantClick());
+        this.empView.getBtnUpdate().addActionListener(e -> updatePlantClick());
+        this.empView.getBtnDelete().addActionListener(e -> deletePlantClick());
+
+        this.empView.getTabPlant().addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                //super.mouseClicked(e);
+                showSelectedRowData();
+            }
+        });
+
+
+    }
+
+    
 }
